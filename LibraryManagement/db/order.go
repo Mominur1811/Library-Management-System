f2e15759f28cf8dfd150d7d1693c8b379804c32a
Mominur1811 <106261454+Mominur1811@users.noreply.github.com>
2024-07-02 18:31:17 +0600

    db query for profile updated

--- LibraryManagement/db/order.go
+++ LibraryManagement/db/order.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"fmt"
 	"librarymanagement/logger"
 	"log/slog"
 	"time"
@@ -13,6 +14,7 @@ type BookRequest struct {
 	BookId        int        `db:"bookid"          json:"bookid"`
 	ReaderId      int        `db:"readerid"        json:"readerid"`
 	IssuedAt      *time.Time `db:"issued_at"       json:"issued_at"`
+	Read_Page     int        `db:"read_page"       json:"read_page"`
 	RequestStatus string     `db:"request_status"  json:"request_status"`
 }
 
@@ -25,6 +27,16 @@ type Request struct {
 	ReaderID       int        `db:"readerid"         json:"reader_id"`
 	IssuedAt       *time.Time `db:"issued_at"        json:"issued_at"`
 	RequestStatus  string     `db:"request_status"   json:"request_status"`
+}
+
+type UserBookHistory struct {
+	RequestId     *int       `db:"request_id"`
+	BookTitle     string     `db:"book_title"       json:"book_title"`
+	Read_Page     int        `db:"read_page"        json:"read_page"`
+	Total_page    int        `db:"total_page"       json:"total_page"`
+	RequestStatus string     `db:"request_status"   json:"request_status"`
+	IssuedAt      *time.Time `db:"issued_at"        json:"issued_at"`
+	Request_dat   *time.Time `db:"created_at"       json:"created_at"`
 }
 
 type BookRequestRepo struct {
@@ -169,6 +181,42 @@ func (r *BookRequestRepo) AcceptRequest(reqId int) error {
 	return nil
 }
 
+func (r *BookRequestRepo) RejectBorrowRequest(reqId int) error {
+
+	updateQry, args, err := GetQueryBuilder().Update(r.Table).
+		Set("request_status", "Rejected").
+		Set("issued_at", time.Now()).
+		Where(sq.Eq{"request_id": reqId}).
+		ToSql()
+	if err != nil {
+		slog.Error(
+			"Failed to create update query for request status",
+			logger.Extra(map[string]any{
+				"error": err.Error(),
+				"query": updateQry,
+				"args":  args,
+			}),
+		)
+		return err
+	}
+
+	// Execute the update query
+	_, err = GetReadDB().Exec(updateQry, args...)
+	if err != nil {
+		slog.Error(
+			"Failed to update of readers active status",
+			logger.Extra(map[string]any{
+				"error": err.Error(),
+				"query": updateQry,
+				"args":  args,
+			}),
+		)
+		return err
+	}
+
+	return nil
+}
+
 func (r *BookRequestRepo) GetBorrowedBooks() ([]*Request, error) {
 
 	qry, args, err := GetQueryBuilder().Select("b.title AS book_title", "b.available AS book_available", "r.name AS reader_username", "rq.request_id", "rq.bookid", "rq.readerid", "rq.issued_at", "rq.request_status").
@@ -204,4 +252,76 @@ func (r *BookRequestRepo) GetBorrowedBooks() ([]*Request, error) {
 	}
 
 	return borrowedBooks, nil
+}
+
+func (r *BookRequestRepo) GetUserHistory(uId *int) ([]*UserBookHistory, error) {
+
+	qry, args, err := GetQueryBuilder().Select("rq.request_id", "b.title AS book_title", "rq.read_page", "b.total_page", "rq.request_status", "rq.issued_at", "rq.created_at").
+		From("book_request rq").
+		Join("book b ON rq.bookid = b.id").
+		Where(sq.Eq{"rq.readerid": *uId}).ToSql()
+
+	if err != nil {
+		slog.Error(
+			"Failed to create Get User history query",
+			logger.Extra(map[string]any{
+				"error": err.Error(),
+				"query": qry,
+				"args":  args,
+			}),
+		)
+		return nil, err
+	}
+
+	history := []*UserBookHistory{}
+	err = GetReadDB().Select(&history, qry, args...)
+	if err != nil {
+		slog.Error(
+			"Failed to Fetch upapproved user",
+			logger.Extra(map[string]any{
+				"error": err.Error(),
+				"query": qry,
+				"args":  args,
+			}),
+		)
+		return nil, err
+	}
+
+	return history, nil
+}
+
+func (r *BookRequestRepo) UpdateUserReadProgress(reqId int, read_today int) error {
+
+	updateQry, args, err := GetQueryBuilder().Update(r.Table).
+		Set("read_page", sq.Expr(fmt.Sprintf("read_page + %d", read_today))).
+		Where(sq.Eq{"request_id": reqId}).
+		ToSql()
+	if err != nil {
+		slog.Error(
+			"Failed to create update query for request status",
+			logger.Extra(map[string]any{
+				"error": err.Error(),
+				"query": updateQry,
+				"args":  args,
+			}),
+		)
+		return err
+	}
+
+	// Execute the update query
+	_, err = GetReadDB().Exec(updateQry, args...)
+	if err != nil {
+		slog.Error(
+			"Failed to update of readers active status",
+			logger.Extra(map[string]any{
+				"error": err.Error(),
+				"query": updateQry,
+				"args":  args,
+			}),
+		)
+		return err
+	}
+
+	return nil
+
 }
